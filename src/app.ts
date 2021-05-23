6a6fddf18450c654720d039da8d1a87e5e9cd88d
ferdie <ferdiebergado@gmail.com>
2021-05-23 14:38:52 +0800

    Removed listener and reorder env module

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,12 @@
 /* eslint-disable node/no-missing-import */
 /* eslint-disable node/file-extension-in-import */
 /* eslint-disable node/no-unsupported-features/es-syntax */
-import './lib/utils/env'
 import express from 'express'
 import cookieParser from 'cookie-parser'
+import './lib/utils/env'
 import { logger, notfound, error as errorHandler } from './lib/middlewares'
-import './lib/auth/listener'
-import authRoutes from './lib/auth/routes'
 import { app as appConfig } from './config'
+import authRoutes from './lib/auth/routes'
 // import documentRoutes from "./app/document/route";
 
 const app = express()
@@ -18,7 +17,7 @@ app.disable('x-powered-by')
 app.use(logger)
 app.use(cookieParser(appConfig.APP_KEY))
 app.use(express.json())
-app.get('/favicon.ico', (_req, res) => res.status(204).end())
+// app.get('/favicon.ico', (_req, res) => res.status(204).end())
 app.use('/auth', authRoutes)
 //app.use("/documents", documentRoutes);
 app.use(notfound)
