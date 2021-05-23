1615b907ce3a83f27a57bec143578198231c28e8
ferdie <ferdiebergado@gmail.com>
2021-05-23 14:40:11 +0800

    Included auth service bootstrapping

--- src/lib/auth/handler.ts
+++ src/lib/auth/handler.ts
@@ -1,17 +1,20 @@
 /* eslint-disable eslint-comments/disable-enable-pair */
-/* eslint-disable node/no-missing-import */
 /* eslint-disable node/file-extension-in-import */
+/* eslint-disable node/no-missing-import */
 /* eslint-disable node/no-unsupported-features/es-syntax */
 import { Request, Response } from 'express'
-import asyncHandler from '../middlewares/asyncHandler'
-import messages from '../../config/messages'
-import { makeUserService } from './'
+import { asyncHandler } from '../middlewares'
+import { messages } from '../../config'
+import { makeRepository } from '../db'
+import { buildMakeAuthService } from './service'
 
-const userService = makeUserService()
+const authRepository = makeRepository('users')
+const makeAuthService = buildMakeAuthService(authRepository)
+const authService = makeAuthService()
 
 export const register = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {
-    const id = await userService.register(req.body)
+    const id = await authService.register(req.body)
     res.status(201).json({ _id: id })
   }
 )
@@ -19,14 +22,14 @@ export const register = asyncHandler(
 export const verify = asyncHandler(
   async (_req: Request, res: Response): Promise<void> => {
     const { sub } = res.locals.jwtPayload
-    await userService.verify(sub)
+    await authService.verify(sub)
     res.json({ message: 'Account verified!' })
   }
 )
 
 export const login = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {
-    const token = await userService.login(req.body)
+    const token = await authService.login(req.body)
     res.cookie('token', token, {
       expires: new Date(Date.now() + 60000 * 30),
       httpOnly: true,
@@ -49,7 +52,7 @@ export const profile = asyncHandler(
 
 export const forgotPassword = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {
-    await userService.forgotPassword(req.body.email)
+    await authService.forgotPassword(req.body.email)
     res.json({ message: messages.passwordResetLinkSent })
   }
 )
@@ -57,7 +60,7 @@ export const forgotPassword = asyncHandler(
 export const reset = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {
     const { sub } = res.locals.jwtPayload
-    await userService.resetPassword(sub.toString(), req.body)
+    await authService.resetPassword(sub.toString(), req.body)
     res.json({ message: messages.passwordChanged })
   }
 )
